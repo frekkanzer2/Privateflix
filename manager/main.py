ab59142e8a33dabb12cb325098feddf151a15447
frekkanzer2 <abatefrancesco98@gmail.com>
2021-03-29 00:47:28 +0200

    Added database updater

---
+++ manager/main.py
@@ -0,0 +1,35 @@
+import json
+
+filepath = input("Insert file path: ")
+
+with open(filepath, "r+") as f:
+    f.seek(0)
+    data = json.load(f)
+    while(True):
+        print("Set category as empty to exit")
+        print("Set title as empty to choise another category")
+        category = input("Select category: ")
+        if category == "":
+            break
+        if category == "azione" or category == "thriller":
+            category = "azione_thriller"
+        elif category == "serie tv":
+            category = "serietv"
+        elif category == "serie tv animata" or category == "serve tv animate":
+            category = "serietv_animate"
+        while(True):
+            print("")
+            title = input("Title: ")
+            if title == "":
+                break
+            videolink = input("Youtube link: ")
+            coverlink = input("Cover link: ")
+            headerlink = input("Header link: ")
+            if title == "annulla" or videolink == "annulla" or coverlink == "annulla" or headerlink == "annulla":
+                continue
+            data['root']['category_film'][category].insert(0, {"title": title, "link": videolink, "imgurl": coverlink, "head": headerlink})
+            f.seek(0)
+            json.dump(data, f, indent=4)
+            f.truncate()
+
+
